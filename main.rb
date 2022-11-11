3f084d3747a82c1a6ec676efa73a76eb95bae38c
iago holek <87425168+FunctionTestIfImGood@users.noreply.github.com>
2022-11-11 04:21:36 +0000

    Criada mais uma classe e alterado o README

--- main.rb
+++ main.rb
@@ -24,7 +24,24 @@ objectDivide = Divide.new(5/2)
 
 objectDivide.print_divide
 objectSum.print_sum
-  
 
+#class pessoa p teste
+Person = Class.new do
+def initialize(user_id, name,age)
+  @person_id   = user_id
+  @person_name = name
+  @person_age  = age
+end                        
+
+define_method :show_person_details do
+  puts "the person id is #@person_id"
+  puts "the person name is #@person_name"
+  puts "the person age is #@person_age"
+end
+end
+
+
+ObjectPerson = Person.new(1, "iago", "24")
+ObjectPerson.show_person_details
   
   
